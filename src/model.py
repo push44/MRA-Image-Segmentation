872bdb18638adb20e56f6ec2d185b26078787c90
Pushkaraj <pushkarajpalnitkar@gmail.com>
2021-09-09 14:27:06 -0400

    Resolved bactchnorm gpu error.

--- src/model.py
+++ src/model.py
@@ -19,15 +19,30 @@ class DeepMedic(nn.Module):
         super(DeepMedic, self).__init__()
         # High resolution
         self.conv1h = nn.Conv3d(in_channels=1, out_channels=30, kernel_size=5, stride=1)
+        self.bn1h = nn.BatchNorm3d(30)
+
         self.conv2h = nn.Conv3d(in_channels=30, out_channels=40, kernel_size=5, stride=1)
+        self.bn2h = nn.BatchNorm3d(40)
+
         self.conv3h = nn.Conv3d(in_channels=40, out_channels=40, kernel_size=5, stride=1)
+        self.bn3h = nn.BatchNorm3d(40)
+
         self.conv4h = nn.Conv3d(in_channels=40, out_channels=50, kernel_size=5, stride=1)
+        self.bn4h = nn.BatchNorm3d(50)
 
         # Low resolution
         self.conv1l = nn.Conv3d(in_channels=1, out_channels=30, kernel_size=5, stride=1)
+        self.bn1l = nn.BatchNorm3d(30)
+
         self.conv2l = nn.Conv3d(in_channels=30, out_channels=40, kernel_size=5, stride=1)
+        self.bn2l = nn.BatchNorm3d(40)
+
         self.conv3l = nn.Conv3d(in_channels=40, out_channels=40, kernel_size=5, stride=1)
+        self.bn3l = nn.BatchNorm3d(40)
+
         self.conv4l = nn.Conv3d(in_channels=40, out_channels=50, kernel_size=5, stride=1)
+        self.bn4l = nn.BatchNorm3d(50)
+
         self.upsampling = nn.Upsample(scale_factor=(4, 4, 4))
 
         # Common layers
@@ -36,7 +51,11 @@ class DeepMedic(nn.Module):
 
         # Final convolutions
         self.convf1 = nn.Conv3d(in_channels=100, out_channels=100, kernel_size=1)
+        self.bn1f = nn.BatchNorm3d(100)
+
         self.convf2 = nn.Conv3d(in_channels=100, out_channels=100, kernel_size=1)
+        self.bn2f = nn.BatchNorm3d(100)
+
         self.convf3 = nn.Conv3d(in_channels=100, out_channels=1, kernel_size=1)
 
 
@@ -46,36 +65,36 @@ class DeepMedic(nn.Module):
         
         # High resolution
         x1 = F.relu(self.conv1h(high_res_img)) # size: (bs, 1, 40, 40, 40)
-        x1 = nn.BatchNorm3d(30)(x1)
+        x1 = self.bn1h(x1)
         x1 = self.dropout1(x1)
 
         x1 = F.relu(self.conv2h(x1)) # size: (bs, 30, 36, 36, 36)
-        x1 = nn.BatchNorm3d(40)(x1)
+        x1 = self.bn2h(x1)
         x1 = self.dropout1(x1)
 
         x1 = F.relu(self.conv3h(x1)) # size: (bs, 40, 32, 32, 32)
-        x1 = nn.BatchNorm3d(40)(x1)
+        x1 = self.bn3h(x1)
         x1 = self.dropout1(x1)
 
         x1 = F.relu(self.conv4h(x1)) # size: (bs, 40, 28, 28, 28)
-        x1 = nn.BatchNorm3d(50)(x1)
+        x1 = self.bn4h(x1)
         x1 = self.dropout1(x1)
 
         # Low resolution
         x2 = F.relu(self.conv1l(low_res_img)) # size: (bs, 1, 22, 22, 22)
-        x2 = nn.BatchNorm3d(30)(x2)
+        x2 = self.bn1l(x2)
         x2 = self.dropout1(x2)
 
         x2 = F.relu(self.conv2l(x2)) # size: (bs, 30, 18, 18, 18)
-        x2 = nn.BatchNorm3d(40)(x2)
+        x2 = self.bn2l(x2)
         x2 = self.dropout1(x2)
 
         x2 = F.relu(self.conv3l(x2)) # size: (bs, 40, 14, 14, 14)
-        x2 = nn.BatchNorm3d(40)(x2)
+        x2 = self.bn3l(x2)
         x2 = self.dropout1(x2)
 
         x2 = F.relu(self.conv4l(x2)) # size: (bs, 40, 10, 10, 10)
-        x2 = nn.BatchNorm3d(50)(x2)
+        x2 = self.bn4l(x2)
         x2 = self.dropout1(x2)
 
         x2 = self.upsampling(x2) # size: (bs, 50, 6, 6, 6)
@@ -88,11 +107,11 @@ class DeepMedic(nn.Module):
 
         # Final convolutions
         x3 = F.relu(self.convf1(x3)) # size: (bs, 100, 24, 24, 24)
-        x3 = nn.BatchNorm3d(100)(x3)
+        x3 = self.bn1f(x3)
         x3 = self.dropout2(x3)
 
         x3 = F.relu(self.convf2(x3)) # size: (bs, 100, 24, 24, 24)
-        x3 = nn.BatchNorm3data_loaderd(100)(x3)
+        x3 = self.bn2f(x3)
         x3 = self.dropout2(x3)
 
         # Output (Dimmension: (bs, 1, 24, 24, 24))
